73b8cfe299b32e890b0c64b899aa4e428d8681c7
anastasia29-step <stephaniataso@gmail.com>
2026-03-11 23:48:34 +0400

    many

--- app.js
+++ app.js
@@ -8,14 +8,6 @@ fetch("https://restaurant.stepprojects.ge/api/Categories/GetAll")
         console.log(data);
         data.forEach((item) => ul.innerHTML += `<li onclick="changeCategory(${item.id})">${item.name}</li>`)
     })
-function changeCategory(id) {
-    right.innerHTML = ""
-    fetch(`https://restaurant.stepprojects.ge/api/Categories/GetCategory/${id}`)
-        .then(pasuxi => pasuxi.json())
-        .then(data => {
-            data.products.forEach((item) => right.innerHTML += card(item))
-        })
-}
 function showAll() {
     fetch("https://restaurant.stepprojects.ge/api/Products/GetAll")
         .then(pasuxi => pasuxi.json())
@@ -23,7 +15,15 @@ function showAll() {
             right.innerHTML = ""
             data.forEach(item => right.innerHTML += card(item))
         })
-        .catch(() => body.innerHTML = `<h1> Error 404...</h1>`)
+        .catch(() => body.innerHTML = `<h1>Oops, something went Wrong...</h1>`)
+}
+function changeCategory(id) {
+    right.innerHTML = ""
+    fetch(`https://restaurant.stepprojects.ge/api/Categories/GetCategory/${id}`)
+        .then(pasuxi => pasuxi.json())
+        .then(data => {
+            data.products.forEach((item) => right.innerHTML += card(item))
+        })
 }
 let sectionCard = document.querySelector(".sectionCard")
 let left = document.querySelector(".leftSide")
@@ -51,8 +51,7 @@ fetch("https://restaurant.stepprojects.ge/api/Products/GetAll")
     })
     .catch(() => body.innerHTML += `<h1> Error 404...</h1>`)
 
-function apply(e) {
-    e.preventDefault()
+function apply() {
     let spiciness = range.value
     let checks = document.querySelectorAll(".check")
     let nuts = checks[0].checked
@@ -67,8 +66,7 @@ function apply(e) {
         })
 
 }
-function reset(e) {
-    e.preventDefault()
+function reset() {
     range.value = 0
     spiceValue.textContent = "Not Chosen"
     let checks = document.querySelectorAll(".check")
@@ -101,9 +99,39 @@ function card(item) {
                 </div>
                 <div class="bottom">
                     <h1 style="font-size: 24px">$ ${item.price} </h1>
-                    <button>Add to cart</button>
+                    <button onclick="addToCart(${item.id}, ${item.price})">Add to cart</button>
                 </div>
             </div>`
+}
+function addToCart(id, price) {
+    let info = {
+        quantity: 1,
+        price: price,
+        productId: id
+    }
+    fetch("https://restaurant.stepprojects.ge/api/Baskets/AddToBasket", {
+        method: "POST",
+        headers: {
+            accept: "text/plain",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(info)
+    }).then((pasuxi) => pasuxi.json())
+        .then((data) => {
+            let existing = item.product.id
+            if (existing) {
+                fetch("https://restaurant.stepprojects.ge/api/Baskets/UpdateBasket", {
+                    method: "PUT",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        id: existing.id,
+                        quantity: existing.quantity + 1
+                    })
+                })
+            }
+        })
 }
 window.addEventListener("scroll", function () {
 
