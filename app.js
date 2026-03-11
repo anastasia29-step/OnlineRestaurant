2999d37505ab0a9fcb492e5f2e49a26e22a15ddf
anastasia29-step <stephaniataso@gmail.com>
2026-03-11 11:58:40 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+let ul = document.querySelector("ul")
+let right = document.querySelector(".rightSide")
+let nav = document.getElementById("navbar")
+
+fetch("https://restaurant.stepprojects.ge/api/Categories/GetAll")
+    .then((pasuxi) => pasuxi.json())
+    .then((data) => {
+        console.log(data);
+        data.forEach((item) => ul.innerHTML += `<li onclick="changeCategory(${item.id})">${item.name}</li>`)
+    })
+
+let sectionCard = document.querySelector(".sectionCard")
+let left = document.querySelector(".leftSide")
+let range = document.querySelector(".range")
+let checks = document.querySelector(".check")
+
+fetch("https://restaurant.stepprojects.ge/api/Products/GetAll")
+    .then(pasuxi => pasuxi.json())
+    .then((data) => {
+        console.log(data);
+        
+        data.forEach((item) => right.innerHTML += card(item))
+    })
+    .catch(() => right.innerHTML += `<h1> Error 404...</h1>`)
+
+function card(item) {
+    return ` <div class="card">
+                <img src="${item.image}" alt="">
+                <h1>${item.name}</h1>
+                <p>Spiciness: ${item.spiciness}</p>
+                <div class="sides">
+                    <div class="checks">
+                        <input type="checkbox" ${item.nuts} ? "checked" : "">
+                        <p>Nuts</p>
+                    </div>
+                    <div class="checks">
+                        <input type="checkbox" ${item.vegeterian} ? "checked" : "">
+                        <p>Vegeterian</p>
+                    </div>
+                </div>
+                <div class="bottom">
+                    <h1 style="font-size: 24px">$ ${item.price} </h1>
+                    <button>Add to cart</button>
+                </div>
+            </div>`
+}
+
+window.addEventListener("scroll", function () {
+
+    if (window.scrollY > 50) {
+        nav.classList.add("active")
+    } else {
+        nav.classList.remove("active")
+    }
+
+})
+
